358912c94678eacfbad486fd959e98d4d55711b4
albim72 <marcin.albiniak72@gmail.com>
2023-05-24 14:07:58 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -36,3 +36,52 @@ rank("Python","Java","C++",nrrank=23,cos = 3253245)
 rank("Python","C#","Java","JavaScript","C++",nrrank=23)
 
 
+#funkcje anonimowe
+#funkcja lambda
+
+print((lambda e:e**3)(4))
+print((lambda e:e**3 if e>10 else e-1)(4))
+print((lambda e:e**3 if e>10 else e-1)(11))
+
+def cos(e):
+    return e**3
+
+print(cos(4))
+
+r = lambda a,b,c:(a+b)*c
+
+print(r(3,7,2))
+print(r(4.55,2.2,1))
+
+def multi(k):
+    return lambda a:a*(k-1)
+
+d = multi(56)
+print(d(9))
+print(multi(18)(7))
+
+
+def druga(k):
+    return k + 1
+
+def innemulti(a,k):
+    return a*druga(k)
+
+print(innemulti(4,7))
+
+mojalista = [n**8 for n in range(10_000_000) if n%2==0] #list comprehension
+print(mojalista)
+sl = sum(mojalista)
+print(sl)
+
+ekstralista = []
+for n in range(10_000_000):
+    if n % 2 == 0 and n%10 == 6:
+        ekstralista.append(n**8)
+
+print(len(ekstralista))
+
+#zastosowania funkcji
+
+lb = [67,9,12,-4,0,112,9,-99,178,12,-2,14,67,55,111,-3]
+
