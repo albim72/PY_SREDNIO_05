fae5eb30019bc08490e190aebc4d20712b1a4659
albim72 <marcin.albiniak72@gmail.com>
2023-05-24 10:55:33 +0200

    Create kolekcje.py

---
+++ DZIEN_1/kolekcje.py
@@ -0,0 +1,28 @@
+print("pierwsza linia na dobry początek")
+
+#komentarz podstawowy - jednoliniowy
+"""
+komentarz wieloliniowy
+"""
+
+kom = """
+drugi
+komentarz
+"""
+
+txt = "jfkdjfk l;fsdldfksdl"
+
+print(kom)
+
+a:int = True
+
+print(a)
+print(type(a))
+
+a = "jedynka"
+
+print(a)
+print(type(a))
+
+
+
