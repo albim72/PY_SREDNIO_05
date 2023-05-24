0ef138c406f1ea4270b7383b2d2c52d230da3234
albim72 <marcin.albiniak72@gmail.com>
2023-05-24 13:19:59 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -1,7 +1,34 @@
+import math
+
 #przykład 1
 
+def f(x):
+    return x**4
+n=100
 def policz(a,b,c,y):
-    n = (a+b)*y-c
+    global n
+    n = (a+b)*y-c + n + f(b)
     return n
 
 print(policz(6,2,5,7))
+print(n)
+
+#przykład 2
+def gx(n,m=1,k=3,b=5):
+    return math.sqrt(n+m)*k-b
+
+print(gx(8,4,1,9))
+print(gx(8,4,1))
+print(gx(8,4))
+print(gx(8))
+print(gx(12,b=6))
+print(gx(7,b=4,k=2))
+
+
+#przykład 3
+def rank(*lang,nrrank):
+    print(f'ranking języków programowania nr {nrrank}, pierwsze miejsce: {lang[0]}, '
+          f'drugie miejsce: {lang[1]}, trzecie miejsce: {lang[2]}')
+
+rank("Python","Java","C++",nrrank=23)
+rank("Python","C#","Java","JavaScript","C++",nrrank=23)
