1fc337c58188909235a420f607ec34c6e2828970
albim72 <marcin.albiniak72@gmail.com>
2023-05-25 10:08:11 +0200

    Update main.py

--- DZIEN_2/mojemoduly/main.py
+++ DZIEN_2/mojemoduly/main.py
@@ -3,6 +3,12 @@
 from dane import nrfilii as filia
 from dane import book as ks
 
+from bfunkcje.kol_funkcje import czytaj_liste,czytaj_slownik
+
 print("_"*15,"dane bezpośrednie","_"*15)
 print(filia)
 print(ks)
+
+print("_"*15,"dane z funkcji","_"*15)
+czytaj_liste(filia)
+czytaj_slownik(ks)
