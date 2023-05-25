e8258167b06f8412d20a37e7dbd1a464e222873d
albim72 <marcin.albiniak72@gmail.com>
2023-05-25 10:03:37 +0200

    Create kol_funkcje.py

---
+++ DZIEN_2/mojemoduly/bfunkcje/kol_funkcje.py
@@ -0,0 +1,8 @@
+def czytaj_liste(lista):
+    for i,j in enumerate(lista):
+        print(f"Element listy nr {i+1} -> {j}.")
+
+
+def czytaj_slownik(slownik):
+    for x,y in slownik.items():
+        print(f"klucz: {x} -> wartość: {y}.")
