22c6f4a41ae48a9a41b3c8ca6c5957dff5d4535e
albim72 <marcin.albiniak72@gmail.com>
2023-05-26 10:06:08 +0200

    Update generatory.py

--- DZIEN_3/generatory.py
+++ DZIEN_3/generatory.py
@@ -28,5 +28,35 @@ def wznowienie(n,k):
     print("wznowienie działania")
 
 for i in wznowienie(8,3):
+    if i == 1008:
+        continue
     print(f"zwrócono wartość: {i}")
+
+print("_"*40)
+
+def complexgen():
+    x = 0
+    while True:
+        print("x-print1")
+        y = yield x
+        print(type(x))
+        print(type(y))
+        print("x-print2")
+        # x = x + 1
+        #warunek if pozwala na wprowadzenie dowolnej wartości x poprzez funkcję send() i kontynuację wyliczania od tej wartości
+        if y is None:
+            x = x+1
+        else:
+            x=y
+
+g = complexgen()
+print(type(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(g.send(121))
+print(next(g))
+print(next(g))
+print(next(g))
+
 
